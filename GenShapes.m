7b8696221f3197210d9b4d635831a47d1be49398
PresleyPlant <michaela.rabenius@gmail.com>
2018-02-19 15:45:40 +0100

    Fixed the images that were wrong and added som functions...

--- GenShapes.m
+++ GenShapes.m
@@ -1,7 +1,15 @@
-img = imread('oval_b.png');
+img1 = imread('shape01.png');
+img2 = imread('oval_a.png');
+img3 = imread('oval_b.png');
+
 %imshow(img);
-im = rgb2gray(im2double(img));
-shape = (im > 0);
+im1 = rgb2gray(im2double(img1));
+im2 = rgb2gray(im2double(img2));
+im3 = rgb2gray(im2double(img3));
+
+circleshape = (im1 > 0);
+blobshape = (im2 > 0);
+blibshape = (im3 > 0);
 
 %imshow(shape);
 
@@ -22,7 +30,7 @@ for col = 10:20:400
     end   
 end
 
-%showRGB(colors);
+showRGB(colors);
 palette = zeros(size(shape));
 
 circles = {};
@@ -31,33 +39,84 @@ for i = 1:171
    palette(:,:,2) = colors(i,2);
    palette(:,:,3) = colors(i,3);
    
+   c = createShape(palette, circleshape);
+   b = createShape(palette, blobshape);
+   d = createShape(palette, blibshape);
    
-   palette(:,:,1) = palette(:,:,1).*shape;
-   palette(:,:,2) = palette(:,:,2).*shape;
-   palette(:,:,3) = palette(:,:,3).*shape;   
-   palette(palette(:,:,:) <= 0) =1;
+   c = whiteBG(c);
+   b = whiteBG(b); 
+   d = whiteBG(d); 
+   %palette = whiteBG(palette);
    %circles{i} = palette;
-   circles{i} = palette;
+   circles{i} = c;
+   blobs{i} = b;
+   blibs{i} = d;
 
-   %subplot(9,19,i), imshow(palette);
+   subplot(9,19,i), imshow(circles{i});
    %truesize
    
 end
-%save('circles.mat', 'circles')
+% blobs = circles;
+save('circles.mat', 'circles')
+save('blobs.mat', 'blobs')
+save('blibs.mat', 'blibs')
 
 
 %%
-
-filename = 'db3/blib_';
+filename1 = 'db1/blob_';
+filename2 = 'db2/blab_';
+filename3 = 'db3/blib_';
 
 for i =1:171
     %subplot(9,19,i), imshow(circles{i});
     
-    name = strcat(filename, int2str(i));
-    name = strcat(name, '.png');
-    imwrite(circles{i}, name);  
+    name1 = strcat(filename1, int2str(i));
+    name1 = strcat(name1, '.png');
+    
+    name2 = strcat(filename2, int2str(i));
+    name2 = strcat(name2, '.png');
+    
+    name3 = strcat(filename3, int2str(i));
+    name3 = strcat(name3, '.png');
+    
+    imwrite(circles{i}, name1);
+    imwrite(blobs{i}, name2);  
+    imwrite(blibs{i}, name3);  
 end
 
+%%
+clear all
+clc
+load blobs
+load blibs
+load circles
 
+for i =1:171
+    subplot(9,19,i), imshow(blobs{i});
+    
+end
+figure
+for i =1:171
+    subplot(9,19,i), imshow(blibs{i});
+    
+end
 
+figure
+for i =1:171
+    subplot(9,19,i), imshow(circles{i});
+end
+
+%%
+
+figure
+for i =1:171
+    b = imrotate(blobs{i},90);
+    subplot(9,19,i), imshow(b);
+end
+%%
+figure
+for i =1:171
+    b = imrotate(blibs{i},90);
+    subplot(9,19,i), imshow(b);
+end
 
