474c37772845fe7cfe799ab2474abf688c177535
Annie Lantz <carla162@student.liu.se>
2018-02-20 16:08:43 +0100

    Prepared for functions to calculate 5 areas forward. Doesn't do anything yet.

--- DiffPixles.m
+++ DiffPixles.m
@@ -1,8 +1,5 @@
 function diff = DiffPixles(prevPix,thisPix)
 % Difference between two pixel
-angleMessage = "Change angle!";
-blobMessage = "Blob time!";
-thres = 0.2;
 
     diff = zeros(size(prevPix));
 
@@ -11,10 +8,6 @@ thres = 0.2;
     end
     
     diff = sum(diff);
-    
-    if(diff > thres)
-        angleMessage;
-    end
-    
+   
 end
 
--- main.m
+++ main.m
@@ -14,8 +14,6 @@ restCol = mod(col,s);
 
 % Crop image
 resultIm = cropIm(img,s,restRow,restCol,row,col);
-prevPix = [0 0 0];
-diff = [0 0 0];
 
 for j = 0:s:row-s
     for i = 0:s:col-s
@@ -28,12 +26,39 @@ for j = 0:s:row-s
         resultIm(j+1:(j+s), i+1:(i+s), 1) = meanr;
         resultIm(j+1:(j+s), i+1:(i+s), 2) = meang;
         resultIm(j+1:(j+s), i+1:(i+s), 3) = meanb;
-        
-        thisPix = [meanr meang meanb];
-        DiffPixles(prevPix,thisPix);
-        prevPix = thisPix;
-        
+      
     end
+end
+
+checkArea = 5; % How far forward to check value.
+
+for j = 0:row-s
+    
+%     counter = 1;
+%     firstr = resultIm((s/2),i*(s/2)+(s/2),1);
+%     firstg = resultIm((s/2),i*(s/2),2);
+%     firstb = resultIm((s/2),i*(s/2),3);
+% 
+%     thisPix = [firstr firstg firstb];
+%     nextPix = [0 0 0];
+%     
+%     for i = 1:col-s
+%            
+%         rval = resultIm(j*s+(s/2)-s, i*s+(s/2)-1, 1);
+%         gval = resultIm(j*s+(s/2)-s, i*s+(s/2)-1, 2);
+%         bval = resultIm(j*s+(s/2)-s, i*s+(s/2)-1, 3);
+%         
+%         nextPix = [rval gval bval];
+%         
+%         if(mod(counter,checkArea) == 0 || counter == 1)
+%             diff = DiffPixles(thisPix,nextPix);
+%             correctShape = correctShape(diff)
+%             thisPix = nextPix;
+%         end
+%         
+%         counter = counter + 1;
+%         
+%     end
 end
 
 imshow(resultIm);
